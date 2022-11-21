41501c8fe89bfb6b06d48f5a91f0e2aa937fbad4
Rajat Sharma <sharma.raja@northeastern.edu>
2022-11-21 17:01:37 -0500

    Add sp GetAdvertisementCountByDock

---
+++ GetAdvertisementCountByDock.sql
@@ -0,0 +1,12 @@
+-- Get Number of Advertisement Shown on Dock by Dates.
+
+CREATE PROCEDURE GetAdvertisementCountByDock @startDate DATE, @endDate DATE 
+AS
+BEGIN
+    SELECT dg.DockID, d.DockName, COUNT(*) AS AdvertisementCount
+    FROM DockGrouping dg JOIN Dock d ON dg.DockID = d.DockID
+    WHERE dg.StartDate >= @startDate AND dg.EndDate <= @endDate
+    GROUP By dg.DockID, d.DockName;
+END
+
+EXEC GetAdvertisementCountByDock '01/01/2020', '12/31/2024';
